d2b1b5ed7ce3012bdcfca698da01f0dfece85590
Gili <gilig42@gmail.com>
2024-03-21 18:16:47 +0200

    CloudFormation changes and build to ECR

--- buildall.sh
+++ buildall.sh
@@ -8,3 +8,15 @@ docker login
 
 docker push gilibee/hello-there-front
 docker push gilibee/hello-there-back
+
+
+#ECR Login
+aws ecr get-login-password --region il-central-1 | docker login --username AWS --password-stdin 058264276766.dkr.ecr.il-central-1.amazonaws.com
+
+#ECR tag
+docker tag hello-there-front:latest 058264276766.dkr.ecr.il-central-1.amazonaws.com/hello-there-front:latest
+docker tag hello-there-back:latest 058264276766.dkr.ecr.il-central-1.amazonaws.com/hello-there-back:latest
+
+#ECR push
+docker push 058264276766.dkr.ecr.il-central-1.amazonaws.com/hello-there-front:latest
+docker push 058264276766.dkr.ecr.il-central-1.amazonaws.com/hello-there-back:latest
